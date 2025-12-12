a4c083c83bddc10fc1c3b8f852453d565607db55
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-12 10:43:48 +0530

    Update bytes to bits.java

--- bytes to bits.java
+++ bytes to bits.java
@@ -5,8 +5,8 @@ public class q24{
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  
-        long bytes = sc.nextInt();
+        long bytes = sc.nextLong();
         long bits = convertBytesToBits(bytes);
-        System.out.println();
+        System.out.println(bits);
     }
 } 
