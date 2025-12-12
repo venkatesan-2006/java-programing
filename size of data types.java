c29d772e14020ab7d08b6b578bbf6990cd95e854
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-12 09:33:45 +0530

    Create size of data types.java

---
+++ size of data types.java
@@ -0,0 +1,8 @@
+public class q21{
+    public static void main(String[] args) {
+        System.out.println("Size of integer: " + Integer.SIZE + " bits");
+        System.out.println("Size of float: " + Float.SIZE + " bits");
+        System.out.println("Size of double: " + Double.SIZE + " bits");
+        System.out.println("Size of char: " + Character.SIZE + " bits");   
+    }
+} 
