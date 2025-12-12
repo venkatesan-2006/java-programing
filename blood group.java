1d4fccad4b5046427119888a8c7b5c9e588689e2
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-12 15:16:04 +0530

    Create blood group.java

---
+++ blood group.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class bg  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter your Blood group: ");
+        String bg = sc.nextLine();
+        System.out.println("Blood group is: " + bg);
+    }
+}
