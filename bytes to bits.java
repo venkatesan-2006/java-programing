6b05d74e8c26dc9ff6ad6a59d2b8bfa37a079c44
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-12 10:39:16 +0530

    Create bytes to bits.java

---
+++ bytes to bits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class q24{
+    public static long convertBytesToBits(long bytes) {
+        return bytes * 8;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        long bytes = sc.nextInt();
+        long bits = convertBytesToBits(bytes);
+        System.out.println();
+    }
+} 
