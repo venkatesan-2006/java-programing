951afe6ae6fc35ef1e63079c36021827c3bcb321
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 00:45:23 +0530

    Update num divisible by 3 and 5.java

--- num divisible by 3 and 5.java
+++ num divisible by 3 and 5.java
@@ -5,6 +5,6 @@ public class bg  {
         System.out.println("Enter a value: ");
         int a = sc.nextInt();
         if(a % 3 == 0 && a % 5 == 0)System.out.println(a + " is divisible by both 3 and 5.");
-        else System.out.println(a + " is not not divisible by both 3 and 5.");
+        else System.out.println(a + " is not divisible by both 3 and 5.");
     }
 }
