ec111a8eb9ca76f797c6b21a78c0365ec97276be
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-12 12:17:56 +0530

    Remove unnecessary whitespace in bytes to bits.java

--- bytes to bits.java
+++ bytes to bits.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
-
 public class BytesToBits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter bytes: ");
         int bytes = sc.nextInt();
-
         int bits = bytes * 8;
-
         System.out.println("Bits = " + bits);
     }
 }
