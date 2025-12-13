449a3d9e10fa0131fe877936b080b19b3bf2c384
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 15:51:57 +0530

    Create cube root of number.java

---
+++ cube root of number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class q8{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A value:");
+        int a = sc.nextInt();
+        double c = a*a*a;
+        System.out.println("Cube root of " + a + " is: " + c);
+
+    }
+}
