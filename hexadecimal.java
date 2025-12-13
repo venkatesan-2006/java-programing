92f482158e5a67bd61fc0ce4c8f7d680c4e138c3
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 12:58:40 +0530

    Create hexadecimal.java

---
+++ hexadecimal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class bg{
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.printf("%x",a);
+    }
+}
