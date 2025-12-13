ef927bb5233091ba9f941981efceeeb93422fd21
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 11:31:16 +0530

    Create perimeter of square.java

---
+++ perimeter of square.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class q16{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side:");
+        int side = sc.nextInt();
+        int perimeter = 4 * side;
+        System.out.println("perimeter of Square is: " + perimeter);
+    }
+}
