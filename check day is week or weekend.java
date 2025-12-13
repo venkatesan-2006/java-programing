4a549d255ce6f3b5f44127592feb2c1410b12274
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 10:33:08 +0530

    Create check day is week or weekend.java

---
+++ check day is week or weekend.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+ public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the day: ");
+     int day = sc.nextInt();
+     switch(day){
+         case 1,2,3,4,5 : 
+             System.out.println(day + " is a weekday.");break;
+         case 6,7 :
+             System.out.println(day + " is weekend.");break;
+         default : 
+         System.out.println("no day");
