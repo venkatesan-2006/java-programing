91642ebfecb098afdeba74486dcd03f78286886a
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 11:40:56 +0530

    Update and rename printing giver word.java to printing given word.java

--- printing given word.java
+++ printing given word.java
@@ -3,7 +3,7 @@ public class bg{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the word: ");
-        String word = sc.nextLine();
+        String word = sc.next();
         System.out.println(word);
         sc.close();
     }
