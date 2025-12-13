cc682261ff87bb7e01625666206c5d8b2d9072a2
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 11:52:26 +0530

    Create print fractional number.java

---
+++ print fractional number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bg{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the fraction number: ");
+        float num = sc.nextFloat();
+        System.out.println(num);
+        sc.close();
+    }
+}
+
