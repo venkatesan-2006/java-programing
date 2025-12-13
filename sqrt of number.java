81392f9ca97c05f60f31d20bef351e957443f55f
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 15:50:42 +0530

    Update and rename q8.java to sqrt of number.java

--- sqrt of number.java
+++ sqrt of number.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 public class q8{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter A value:");
+        System.out.println("Enter a value:");
         int a = sc.nextInt();
         double b = a*a;
-        double c = a*a*a;
         System.out.println("Square root of " + a + " is: " + b);
-        System.out.println("Cubic root of " + a + " is: " + c);
-
     }
+
 }
