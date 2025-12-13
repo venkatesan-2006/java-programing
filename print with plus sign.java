71da863b9c713a25216c4cff32c6c383b61b2e1b
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 14:50:20 +0530

    Update print with plus sign.java

--- print with plus sign.java
+++ print with plus sign.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-class bg {
+public class bg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        System.out.println("+" + a);
+        System.out.printf("+%d",a);
 
     }
 }
