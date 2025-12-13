1b8adaca274be7d1a9f622179f0c2d948f4ee92d
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 11:47:05 +0530

    Create print given integer.java

---
+++ print given integer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bg{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the integer: ");
+        int num = sc.nextInt();
+        System.out.println(num);
+        sc.close();
+    }
+}
+
