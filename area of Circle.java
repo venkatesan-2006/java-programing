6239ce3dd178ba8b7fe0b9f26cd1c01811253991
venkatesan-2006 <venkatesanmahalingam2006@gmail.com>
2025-12-13 11:27:23 +0530

    Update and rename Circle.java to area of Circle.java

--- area of Circle.java
+++ area of Circle.java
@@ -4,9 +4,8 @@ public class q17{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Radius:");
         int Radius = sc.nextInt();
-        double circumference = (2 * 3.14 * Radius);
         double area = (3.14 * Radius * Radius);
         System.out.println("Area of circle is: "+ area);
-        System.out.println("Circumference of circle is: "+ circumference);
     }
 }
+
